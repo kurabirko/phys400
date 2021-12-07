e32d3f796e58764a65bb712f2a9f408cb2430998
Utku Birkan <utku.birkan@gmail.com>
2021-12-08 00:21:46 +0300

    [CI] Simplify the whole process

--- shared/common/githubSha.lua
+++ shared/common/githubSha.lua
@@ -1,20 +1,16 @@
 local M = {}
 
 function M.print()
-  local ref_name = os.getenv('GITHUB_REF_NAME') or ''
-  if ref_name:find('^refs/tags/release-') ~= nil then
+  local is_ci = os.getenv('CI') or ''
+  if is_ci ~= 'true' then
     return
   end
   
-  tex.sprint('\\begingroup Build: \\ttfamily ')
-
   local version = os.getenv('GITHUB_SHA')
-  if version == nil then
-    version = 'local'
-  else
-    version = string.sub(version, 1, 7)
-  end
-  tex.sprint(version .. ' \\endgroup' )
+
+  tex.sprint '\\begingroup Build: \\ttfamily ' ..
+    string.sub(version, 1, 7) ..
+    ' \\endgroup'
 end
 
 return M
