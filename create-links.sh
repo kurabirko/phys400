f91f7c17af86c9b4f92f80d9c18a21bc67a28252
Utku Birkan <utku.birkan@gmail.com>
2021-12-07 23:44:00 +0300

    Restructure

--- create-links.sh
+++ create-links.sh
@@ -1,9 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-COMMON_DIR="common"
+# To retain portability between macos and linux, use bash 3
+docs=('proposal' 'interim' 'final' 'poster')
+links=( 'common:common' 'vscode:.vscode' )
 
-for dir in src/*; do
-  if [ "$dir" != "src/$COMMON_DIR" -a $dir != "src/build" ]; then
-    ln -s "../$COMMON_DIR" "$dir/$COMMON_DIR"
-  fi
+for doc in "${docs[@]}"; do
+  for i in "${links[@]}"; do
+    source="../../shared/${i%%:*}"
+    target="documents/$doc/${i##*:}"
+
+    mkdir -p $target
+    ln -s $source $target
+  done
 done
+
